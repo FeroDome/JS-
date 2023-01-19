10570c4ef049191c3cfdbb0b4de8df5754a3ea82
Fero Dome <infoferodome@hotmail.com>
2023-01-19 20:07:14 -0300

    correcciones

--- main.js
+++ main.js
@@ -62,7 +62,7 @@
 
 //    } else {
 //        alert("No Tenemos Ese Producto")
-    
+
 //    }
 //    seleccion = prompt("Desea Agregar otro Producto?")
 //    while (seleccion === "no") {
@@ -77,44 +77,44 @@
 
 //}
 
-const productos = ¨[
+const productos = [
     {
-        id: 1;
-        nombre: "Cerveza";
-        precio: 200;
-        img: 
-        "https://media.ambito.com/p/f91439d1dece9caf73e785dbc7246749/adjuntos/239/imagenes/040/157/0040157125/cervezajpg.jpg";
-        
-    };
+        id: 1,
+        nombre: "Cerveza",
+        precio: 200,
+        img:
+            "https://media.ambito.com/p/f91439d1dece9caf73e785dbc7246749/adjuntos/239/imagenes/040/157/0040157125/cervezajpg.jpg",
+
+    },
 
     {
-        id: 2;
-        nombre: "Mani"
-        precio: 50;
+        id: 2,
+        nombre: "Mani",
+        precio: 50,
         img:
-        "https://www.saborusa.com/do/wp-content/uploads/sites/8/2020/05/7-poderosas-razones-para-comer-mani-Foto-destacada.png";
+            "https://www.saborusa.com/do/wp-content/uploads/sites/8/2020/05/7-poderosas-razones-para-comer-mani-Foto-destacada.png",
 
-    };
+    },
     {
-        id: 3;
-        nombre: "Fernet"
-        precio: 700;
+        id: 3,
+        nombre: "Fernet",
+        precio: 700,
         img:
-        "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/064/758/products/fernet-branca4-811ab044600503c27f16060031316677-480-0.jpg";
+            "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/064/758/products/fernet-branca4-811ab044600503c27f16060031316677-480-0.jpg",
 
-    };
+    },
     {
-        id: 4;
-        nombre: "Coca-cola"
-        precio: 300;
+        id: 4,
+        nombre: "Coca-cola",
+        precio: 300,
         img:
-        "https://imagenes.20minutos.es/files/image_990_v3/uploads/imagenes/2022/08/25/coca-cola.jpeg";
-    };
+            "https://imagenes.20minutos.es/files/image_990_v3/uploads/imagenes/2022/08/25/coca-cola.jpeg",
+    },
 ];
 
 let carrito = [];
 
-productos.array.forEach(product) => {
+productos.array.forEach((product) => {
     let content = document.createElement("div");
     content.innerHTML = `
     <img src="${product.img}">
